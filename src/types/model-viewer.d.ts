a9595b454ae57367f05cc1770b9731175d7cc33d
Stephen Gault <stephengault626@googlemail.com>
2024-11-09 20:25:46 +0000

    types fix

--- src/types/model-viewer.d.ts
+++ src/types/model-viewer.d.ts
@@ -1,3 +1,4 @@
+// model-viewer.d.ts
 declare namespace JSX {
     interface IntrinsicElements {
       'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
@@ -6,6 +7,11 @@ declare namespace JSX {
         'auto-rotate'?: boolean;
         'camera-controls'?: boolean;
         'environment-image'?: string;
+        exposure?: string;
+        'shadow-intensity'?: string;
+        slot?: string;
+        'data-position'?: string;
+        'data-normal'?: string;
       };
     }
   }
